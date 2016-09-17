f0f1e81fc2ffbfb1a55dd710cf3d3660d550f86e
Iatus <geovannimartins@ic.ufal.br>
2016-09-17 14:59:29 -0300

    Just a test

---
+++ STL/O.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Person {
+	string name;
+	int day, month, year;
+	
+	Person(string n, int d, int m, int y) 
+	: name(n), day(d), month(m), year(y) {}
+
+	bool operator < (const Person& str) const {
+		if(year > str.year) return true;
+		if(year == str.year) {
+			if(month > str.month) return true;
+			if(month == str.month) {
+				if(day > str.day) return true;
+				if(day == str.day)
+					if(name > str.name) return true;
+			}
+		}
+		return false;
+	}
+
+	bool operator > (const Person& str) const {
+		if(year < str.year) return true;
+		if(year == str.year) {
+			if(month < str.month) return true;
+			if(month == str.month) {
+				if(day < str.day) return true;
+				if(day == str.day)
+					if(name < str.name) return true;
+			}
+		}		return false;
+	}
+};
+
+int main() {
+	int n;
+	cin >> n;
+	vector<Person> vec;
+	for(int i = 0; i < n; ++i) {
+		string n;
+		int d, m, y;
+		cin >> n >> d >> m >> y;
+		vec.push_back(Person(n, d, m, y));
+	}
+
+	sort(vec.begin(), vec.end(), greater<Person>());
+
+	cout << vec[n-1].name << endl << vec[0].name << endl;
+	return 0;
+}
+
